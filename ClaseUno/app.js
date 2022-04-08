a852d1cb9c142ea3252b360493979f9900b45a73
Nicolas Cordoba <nicocordoba94@gmail.com>
2022-04-07 21:02:15 -0300

    Primera subida del archivo de JS

---
+++ ClaseUno/app.js
@@ -0,0 +1,10 @@
+/* let entrada = prompt("Ingresar una letra");
+let salida  = entrada + " " + "ingresada";
+alert(salida); */
+
+let num1 = parseInt(prompt("Ingrese un numero para sumar: "));
+let num2 = parseInt(prompt("Ingrese otro numero para sumar con el anterior: "));
+
+let resultado = num1 + num2;
+
+alert(resultado);
